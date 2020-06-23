6692c548d8d338017efb972b80c908a4845dcb03
FAYEZKASSEM0 <67088058+FAYEZKASSEM0@users.noreply.github.com>
2020-06-23 22:31:23 +0200

    Update Task21.java

--- Task21.java
+++ Task21.java
@@ -5,31 +5,23 @@ import java.util.Scanner;
 
 public class Task21 {
 
-    // Write a program that take from user number consist of many digits and then get count of every number between 1 – 9
     public static void main(String[] args) {
         System.out.print("please enter a number: ");
         Scanner t = new Scanner(System.in);
         long num = t.nextLong();
-        int nums [] = new int [9];
-        int empty=0, full=0, count=0;
-        for (int i = 0; i<9; i++){
-            int dig = (int) (num % 10);
-            nums[i] = dig;
-            num = num/10;
+        ArrayList<Integer> nums = new ArrayList<>();
+        int dig;
+        for (int i = 0; i < 9; i++) {
+            dig = (int) (num % 10);
+            nums.add(dig);
+            num = num / 10;
         }
- 
-        for (int i = 0; i<9; i++){
-            if(nums[i] == 0)
-                empty = empty + 1;
+        int size = nums.size(), count = 1;
+        for (int i = 0; i < size; i++) {
+            count = count + 1;
+            System.out.println(count + " .... > " + nums.get(i));
         }
-        full = 9-empty;
-        
-        for (int i = 0; i<full; i++){
-            count = i+1;
-            System.out.println( count + " .... > " + nums[i]);
-        }
-       
 
     }
-    
+
 }
